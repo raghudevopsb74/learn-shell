3e361ec30de6c842c1afae6e5faba5001bb854d8
Raghu K <raghuk.vit@gmail.com>
2023-07-25 06:23:07 +0530

    Password NFR

--- 04-functions.sh
+++ 04-functions.sh
@@ -8,3 +8,15 @@ greeting() {
 
 greeting
 echo Function Exit Status - $?
+
+# You declare var in main program, you can access that in function and vice-versa.
+# Function have its own special variables
+
+input() {
+  echo First Argument - $1
+  echo Second Argument - $2
+  echo All Arguments - $*
+  echo No of Arguments - $#
+}
+
+input abc 1234
