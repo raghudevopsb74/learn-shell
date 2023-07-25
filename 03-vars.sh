e5a2a1cd5cd5ea54f0752aeb69179ec5b785b5f1
Raghu K <raghuk.vit@gmail.com>
2023-07-25 06:15:53 +0530

    Password NFR

--- 03-vars.sh
+++ 03-vars.sh
@@ -1,5 +1,18 @@
+# Variables
 a=10
 echo a is  $a
 
+# Special Variables
+# $0 - $N, $*, $#
+
+# Substitution Variables
+## Command Substitution
+DATE=$(date)
+
+echo Today Date is $DATE
+
+## Arithmetic Substitution
+ADD=$(( 2+2 ))
+echo ADD of 2+2 = $ADD
 
 
