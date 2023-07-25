1b2b7835e166fb6a1a5ff46d7ee39555632e6e75
Raghu K <raghuk.vit@gmail.com>
2023-07-25 06:33:43 +0530

    Password NFR

--- 07-loops.sh
+++ 07-loops.sh
@@ -6,5 +6,12 @@ a=10
 while [ $a -gt 0 ]; do
   echo Hello
   a=$(($a-1))
+  #break # this command can break the loop
 done
+
+# Based on Inputs
+for comp in frontend catalogue user ; do
+  echo Installing Component - $comp
+done
+
 
